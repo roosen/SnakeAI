3ac924990eb7f82104580e0feaa9658657fa9a31
Henri Roosen <henriroosen@gmail.com>
2021-01-25 00:19:25 +0100

    add game observation

--- snake.py
+++ snake.py
@@ -1,4 +1,4 @@
-import random, image, time, lcd, array, utime
+import random, image, time, lcd, array, utime, math
 from collections import namedtuple
 
 
@@ -30,6 +30,7 @@ class Game:
         self.draw_dot(self.apple, COLOR_APPLE)
         for pt in self.snake:
             self.draw_dot(pt, COLOR_SNAKE)
+        self.vision()
         lcd.display(self.img)
 
 
@@ -73,12 +74,38 @@ class Game:
             lost_tail = self.snake.pop()
             self.draw_dot(lost_tail, COLOR_BACKGROUND)
 
+        self.vision()
         lcd.display(self.img)
         return False, self.score
 
 
     def draw_dot(self, point, color):
         self.img.draw_circle((point.x * BLOCK_SIZE) + (BLOCK_SIZE // 2), (point.y * BLOCK_SIZE) + (BLOCK_SIZE // 2), BLOCK_SIZE // 2, color, fill=True)
+
+
+    def vision(self):
+        # look left, front, right and determine the distance to the first obstacle
+        x_same_y = [s.x for s in self.snake if s.y == self.head.y]
+        x_same_y.sort()
+        x_same_y.insert(0, -1)
+        x_same_y.append(self.w)
+        i_head_x = x_same_y.index(self.head.x)
+        dx_l = self.head.x - x_same_y[i_head_x - 1]
+        dx_r = x_same_y[i_head_x + 1] - self.head.x
+        y_same_x = [s.y for s in self.snake if s.x == self.head.x]
+        y_same_x.sort()
+        y_same_x.insert(0, -1)
+        y_same_x.append(self.h)
+        i_head_y = y_same_x.index(self.head.y)
+        dy_u = self.head.y - y_same_x[i_head_y - 1]
+        dy_d = y_same_x[i_head_y + 1] - self.head.y
+        d = [dx_r, dy_d, dx_l, dy_u]
+        front = d[self.direction]
+        right = d[(self.direction + 1) % 4]
+        left = d[(self.direction - 1) % 4]
+        # look how far the apple is
+        d_apple = math.sqrt(((self.head.x - self.apple.x) ** 2) + ((self.head.y - self.apple.y) ** 2))
+        self.observation = [1/left, 1/front, 1/right, 1/d_apple]
 
 
 if __name__ == '__main__':
@@ -93,6 +120,7 @@ if __name__ == '__main__':
             game_over, score = game.play(action)
             if game_over:
                 break;
+            print(game.observation)
         lcd.clear()
         lcd.draw_string(lcd.width() // 4, lcd.height() // 2, "GAME OVER", lcd.RED, lcd.BLACK)
         lcd.draw_string(lcd.width() // 4, lcd.height() * 2 // 3, "Score: {}".format(score), lcd.RED, lcd.BLACK)
